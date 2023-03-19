9926f40e3b834137d4eff1684a034ffc5c1653e9
usharik <usharik@mail.ru>
2023-03-19 20:12:22 +0100

    Init

---
+++ src/test/java/ru/phonebook/SptingPhonebookApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.phonebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SptingPhonebookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
